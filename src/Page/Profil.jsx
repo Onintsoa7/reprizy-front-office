d7aa8043b3ea8d89d38a4ba4727947f9af51768f
ETU1767 <tsoatsiafoy@gmail.com>
2024-02-09 21:04:55 +0300

    profil

--- src/Page/Profil.jsx
+++ src/Page/Profil.jsx
@@ -1,70 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../Component/Header';
 import Footer from '../Component/Footer';
+import { Image } from 'primereact/image';
+import { get } from "../axios_utils"
+import Url from '../Url';
+import { Link } from 'react-router-dom';
+// import 'primeflex/primeflex.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "./profil.css"
 function Profil() {
+  const [dataClient, setDataClient] = useState([]);
+  const [annonce, setAnnonce] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setDataClient(JSON.parse(localStorage.getItem('user')));
+        const [response] = await Promise.all([
+          get(Url + "Annonces/myAnnonces")
+        ]);
+        // console.log(Url + "Annonces/client/" + dataClient.id);
+        // console.log(response.data.data[0]);
+        setAnnonce(response.data.data[0]);
+      } catch (error) {
+        console.error("donnee pas arriver" + error);
+      }
+    }
+    fetchData();
+    
+  }, []);
+  // console.log(dataClient.id);
+  // console.log(Url);
+  console.log(annonce);
+  // console.log("annonce :" + annonce);
   return (
-    <div>
-        <Header/>
-        <Profil_contenu/>
-      <Footer />
+    <div className='card'>
+      <div className="card_inner">
+        <div className="user-profil">
+          <div className="image-container">
+            <Image className='sary circular-image' src={dataClient.image} alt="loading.." preview />
+            <br />
+            <h2>{dataClient.prenom} {dataClient.nom}</h2>
+            <hr className='ligne' />
+
+          </div>
+          <br />
+          <div className="detail_user">
+            <div className="text">
+              {/* <p style={{color:'black'}}>CIN: {dataClient.cin}</p> */}
+              <p style={{ color: 'black' }}>Vente :{dataClient.vente || " Aucune vente reussie"}</p>
+              <p style={{ color: 'black' }}>Annonce :{dataClient.annonce || " Aucune annonce publie"}</p>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div className="card_inner2">
+          <h2>Listes des annonces</h2>
+          <div className="place_annonce">
+    <div className="annonce-container">
+        {annonce.map((item, index) => (
+          
+            <div className="annonce" key={index}>
+              
+                <div className="image">
+                    <img src={item.images[0]} style={{width:'100px',height:'100px'}} alt="" />
+                </div>
+                <div className="description">
+                <h2><Link to={'/Detail/' + item.id}>{item.libelle}</Link></h2>
+                  <p style={{fontSize:'12 pt'}}>Etat : {item.etatVehicule.nom}</p>
+                  <br />
+                  <p style={{fontSize:'8pt'}}>
+                  Prix : {item.prix} Ar {item.etatAnnonce === 10 ? 'Valider' : (item.etatAnnonce === 0 ? 'validation en attente' : (item.etatAnnonce === 20 ? 'vendue' : 'valider'))}
+                  </p>
+
+                </div>
+            </div>
+        ))}
+    </div>
+</div>
+      </div>
     </div>
   )
-}
-
-function Profil_contenu(){
-
-    const actu = [
-        {
-          logo: "logo-html5",
-          level: "Advance",
-          count: 86,
-        },
-        {
-          logo: "logo-css3",
-          level: "Expect",
-          count: 90,
-        },
-        {
-          logo: "logo-nodejs",
-          level: "Beginner",
-          count: 40,
-        },
-        {
-          logo: "logo-react",
-          level: "Intermediate",
-          count: 80,
-        },
-      ];
-    return (
-      <section id="skills" className="py-10 bg-gray-800 relative">
-        <div className="mt-8 text-gray-100 text-center">
-          <h3 className="text-4xl font-semibold">
-            My <span className="text-cyan-600">Skills</span>
-          </h3>
-          <p className="text-gray-400 mt-3 text-lg">My knowledge</p>
-          <div className="flex items-center justify-center mt-12 gap-10 flex-wrap">
-            {actu?.map((skill, i) => (
-              <div
-                key={i}
-                className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
-              >
-                <div
-                  style={{
-                    background: `conic-gradient(rgb(8,145,170) ${skill.count}%,#ddd ${skill.count}%)`,
-                  }}
-                  className="w-32 h-32 flex items-center justify-center rounded-full"
-                >
-                  <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center group-hover:text-cyan-600">
-                    <ion-icon name={skill.logo}></ion-icon>
-                  </div>
-                </div>
-                <p className="text-xl mt-3">{skill.level}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-    )
 }
 
 export default Profil
---
+++ src/Url.jsx
@@ -0,0 +1,1 @@
+export default "https://repr-izy-production.up.railway.app/api/v1/"
